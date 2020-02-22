94f7f3c51d3be683266a86f1a7274d1d081cf27f
Bradley Burcar <btburcar@gmail.com>
2020-02-22 01:32:47 -0500

    Add project files.

---
+++ Generic Template/ModSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BattleTech;
+
+namespace General_Tools
+{
+    public class ModSettings
+    {
+        public bool Debug = false;
+        public string modDirectory;
+    }
+}
